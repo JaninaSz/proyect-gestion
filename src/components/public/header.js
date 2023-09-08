eff5c2ecac8e6b75aebef811a7979a92d92ccb58
= <=>
2023-09-07 21:24:07 -0300

    Base

--- src/components/public/header.js
+++ src/components/public/header.js
@@ -4,9 +4,10 @@ export const Header = () => {
     <div>
 <div class="cont-header" id="cont-header">
             <span>
-                <a href="/"> <img src="public/icon.png" alt="" /></a>
+            <a href="/"> <img src="/public/gestion.png" alt="logo" />
+            </a>
             </span>
-
+            <img src="/public/icon.png" alt="logo" />
             <ul>
                 <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
                 <li class="nav-item"><a href="#2" class="nav-link">Detalles</a></li>
